0c1d8d684507f86f16966a81ec98df17bcc58a9c
Robert Csakany <csakany.robert@gmail.com>
2015-08-06 03:36:41 +0200

    dsfsdf

--- ci/create_settings.sh
+++ ci/create_settings.sh
@@ -2,3 +2,6 @@
 #mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version
 #export MAVEN_PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "\[")
 #echo  $MAVEN_PROJECT_VERSION
+
+MAVEN_PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
+echo  "Maven version: $MAVEN_PROJECT_VERSION"s
